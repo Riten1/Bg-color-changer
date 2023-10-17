110fba686ccbcbfe1c957c8af5cae41e90debe6a
Riten1 <122423532+Riten1@users.noreply.github.com>
2023-10-17 11:52:07 +0545

    'built'

---
+++ colorChanger/src/components/body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Buttons from './colorButtons'
+import { useState } from 'react'
+
+const Body = () => {
+  const [color, setColor] = useState('white');
+  return (
+    <div className='w-full h-screen duration-200' style={{background: color}} >
+      <Buttons colorSet = {setColor}/>
+    </div>
+  )
+}
+
+export default Body
